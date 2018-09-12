322f84cefafe40332f8d55456db19bbd99ce6a87
razh <razh@users.noreply.github.com>
2018-09-12 10:06:55 -0400

    Add lerp() function

--- src/math.js
+++ src/math.js
@@ -2,6 +2,10 @@ export var clamp = (value, min, max) => {
   return Math.min(Math.max(value, min), max);
 };
 
+export var lerp = (x, y, t) => {
+  return (1 - t) * x + t * y;
+};
+
 export var randFloatSpread = range => {
   return range * (0.5 - Math.random());
 };
